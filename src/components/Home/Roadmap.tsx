083034f274282367123276bb3bb9599aec935413
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-05-28 17:53:47 +0500

    roadmap-done

---
+++ src/components/Home/Roadmap.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import ComponentWrapper from '../common/ComponentWrapper';
+import RoadMapCard from './Cards/RoadMapCard';
+
+const Roadmap: React.FC = () => {
+  return (
+    <div className='w-full mt-28 pt-20'>
+      <ComponentWrapper>
+        <div className='w-full flex flex-col justify-center items-center'>
+          <p className='text-[14px] font-gesistMedium text-green-1'>
+            */ Project Roadmap
+          </p>
+          <h2 className='text-white-1 mt-5 text-center max-w-[800px] leading-[38px] sm:leading-[44px] w-full text-[28px] sm:text-[36px] font-gesistMedium'>
+            From Vision to Reality - Charting the Course with Our Project
+            Roadmap
+          </h2>
+          <div className='w-full lg:grid hidden gap-6 grid-cols-2 lg:grid-cols-3 mt-16'>
+            {roadMapData.slice(0, 3).map((item, index) => {
+              return (
+                <RoadMapCard
+                  index={index}
+                  key={index}
+                  list={item.list}
+                  title={item.title}
+                />
+              );
+            })}
+          </div>
+          <div className='w-full lg:hidden grid gap-4 csm:gap-6 grid-cols-1 csm:grid-cols-2 lg:grid-cols-3 mt-16'>
+            {roadMapData.map((item, index) => {
+              return (
+                <RoadMapCard
+                  index={index}
+                  key={index}
+                  list={item.list}
+                  title={item.title}
+                />
+              );
+            })}
+          </div>
+          <div className='w-full lg:grid hidden gap-6 grid-cols-2 mt-6'>
+            {roadMapData.slice(3, 5).map((item, index) => {
+              return (
+                <RoadMapCard
+                  index={index + 3}
+                  key={index}
+                  list={item.list}
+                  title={item.title}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </ComponentWrapper>
+    </div>
+  );
+};
+
+const roadMapData = [
+  {
+    title: 'Enhance Existing Features',
+    list: [
+      'Focus on refining and optimizing current monitoring tools like Summit Seeker and Insider Tracker.',
+      'Incorporate user feedback to improve the interface and functionality.',
+    ],
+  },
+  {
+    title: 'Launch of Pump Checker',
+    list: [
+      'Pump Checker bridges crucial gaps in the market, providing thorough analysis and comprehensive insights tailored for PumpFun traders, surpassing tools like RickBot.',
+    ],
+  },
+  {
+    title: 'Strategic Partnerships',
+    list: [
+      'Develop partnerships with key players in the web3 space to enhance visibility and integration.',
+      'Engage with influential communities to expand the network and user base.',
+    ],
+  },
+  {
+    title: 'Expand Community Engagement',
+    list: [
+      'Implement community-driven initiatives and incentives to increase user engagement.',
+      'Organize webinars and workshops to educate and integrate new users into the PumpFun ecosystem.',
+    ],
+  },
+  {
+    title: 'New Features Based on Market Trends',
+    list: [
+      "Continuously adapt and introduce new features based on user feedback and emerging market trends to maintain the service's competitive edge. We ensure our offerings remain cutting-edge and aligned with evolving customer needs.",
+    ],
+  },
+];
+
+export default Roadmap;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from '../components/Home/HeroSection';
 import Features from '../components/Home/Features';
 import Tokenomics from '../components/Home/Tokenomics';
+import Roadmap from '../components/Home/Roadmap';
 
 const Home: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const Home: React.FC = () => {
       <HeroSection />
       <Features />
       <Tokenomics />
+      <Roadmap />
     </div>
   );
 };
